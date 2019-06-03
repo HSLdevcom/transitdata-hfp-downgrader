16ffcae5381c1a99d3bc45043961f748afd79a59
Kenta Kusumoto <kenta.kusumoto@futurice.com>
2019-06-03 09:42:29 +0300

    Revert "Merge pull request #8 from HSLdevcom/feat/disable-downgrader"
    This reverts commit d9dadcfa0e479ad9ab12b12172bcc7933e939d07, reversing
    changes made to 1dfdd2d8ce5d70862446340a7f58b1546a4d2745.

--- src/main/java/fi/hsl/transitdata/hfp/downgrader/Main.java
+++ src/main/java/fi/hsl/transitdata/hfp/downgrader/Main.java
@@ -6,9 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Optional;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
 
@@ -17,49 +14,44 @@ public class Main {
     public static void main(String[] args) {
         log.info("Launching transitdata-hfp-downgrader");
 
-        //MqttConnector connectorIn = null;
-        //MqttConnector connectorOut = null;
+        MqttConnector connectorIn = null;
+        MqttConnector connectorOut = null;
 
         try {
-            //final Config config = ConfigParser.createConfig();
-            //final Optional<Credentials> credentialsIn = Credentials.readMqttCredentials(config, "mqtt-broker-in");
-            //final Optional<Credentials> credentialsOut = Credentials.readMqttCredentials(config, "mqtt-broker-out");
+            final Config config = ConfigParser.createConfig();
+            final Optional<Credentials> credentialsIn = Credentials.readMqttCredentials(config, "mqtt-broker-in");
+            final Optional<Credentials> credentialsOut = Credentials.readMqttCredentials(config, "mqtt-broker-out");
 
-            //log.info("Configurations read, connecting.");
+            log.info("Configurations read, connecting.");
 
-            //connectorIn = new MqttConnector(config, "mqtt-broker-in", credentialsIn);
-            //connectorOut = new MqttConnector(config, "mqtt-broker-out", credentialsOut);
+            connectorIn = new MqttConnector(config, "mqtt-broker-in", credentialsIn);
+            connectorOut = new MqttConnector(config, "mqtt-broker-out", credentialsOut);
 
-            //final MessageProcessor processor = new MessageProcessor(config, connectorIn, connectorOut);
+            final MessageProcessor processor = new MessageProcessor(config, connectorIn, connectorOut);
             //Let's subscribe to connector before connecting so we'll get all the events.
-            //connectorIn.subscribe(processor);
+            connectorIn.subscribe(processor);
 
-            //connectorOut.connect();
-            //final long now = System.currentTimeMillis();
-            //final long timeout = 10000; // 10 seconds
-            //while (!connectorOut.client.isConnected()) {
-            //    if (System.currentTimeMillis() - now > timeout) {
-            //        throw new Exception("Failed to connect MQTT client (out) within timeout");
-            //    }
-            //    Thread.sleep(1000);
-            //}
-            //connectorIn.connect();
+            connectorOut.connect();
+            final long now = System.currentTimeMillis();
+            final long timeout = 10000; // 10 seconds
+            while (!connectorOut.client.isConnected()) {
+                if (System.currentTimeMillis() - now > timeout) {
+                    throw new Exception("Failed to connect MQTT client (out) within timeout");
+                }
+                Thread.sleep(1000);
+            }
+            connectorIn.connect();
 
-            final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
-            scheduler.scheduleAtFixedRate(() -> {
-                log.info("Doing nothing");
-            }, 0, 3600, TimeUnit.SECONDS);
-
-            //log.info("Connections established, let's process some messages");
+            log.info("Connections established, let's process some messages");
         }
         catch (Exception e) {
             log.error("Exception at main", e);
-            //if (connectorIn != null) {
-            //    connectorIn.close();
-            //}
-            //if (connectorOut != null) {
-            //    connectorOut.close();
-            //}
+            if (connectorIn != null) {
+                connectorIn.close();
+            }
+            if (connectorOut != null) {
+                connectorOut.close();
+            }
         }
     }
 }
